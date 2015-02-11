4cc74aab52609689bf0d8e406ec5dd1aad7a359d
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-02-11 10:22:36 -0500

    Updatd Deck class - INCOMPLETE

--- COMP123-Lesson6-Part3/Deck.cs
+++ COMP123-Lesson6-Part3/Deck.cs
@@ -15,25 +15,36 @@ namespace COMP123_Lesson6_Part3
         // CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++++++++++++++++
         public Deck()
         {
-            this._addCard(0, Face.ACE, Suits.HEARTS);
 
-
-
-
-
-
-
-
-
-
-
-
+            this._buildDeck();
         }
 
         // PRIVATE METHODS +++++++++++++++++++++++++++++++++++++++++++++++++
         private void _addCard(int card, int face, string suit)
         {
             this._cards[card] = new Card(face, suit);
+        }
+
+        private void _buildDeck()
+        {
+            string[] suits = { Suits.CLUBS, Suits.DIAMONDS, Suits.HEARTS, Suits.SPADES };
+
+
+            for (int suit = 0; suit < suits.Length; suit++)
+            {
+
+                for (int face = 1; face < 14; face++)
+                {
+                    this._addCard(suit, face, suits[suit]);
+                }
+
+
+                    
+
+            }
+
+
+                
         }
 
 
